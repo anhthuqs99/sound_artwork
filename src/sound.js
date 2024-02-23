f226f554a02b36723669fcddf9703a3ce30f51cd
anhthuqs99 <anhthuptqs99@gmail.com>
2024-02-23 17:35:27 +0700

    update contract address & contract ABI

--- src/sound.js
+++ src/sound.js
@@ -1,5 +1,5 @@
 // This contract is on testnet, update to the PRD contract address after deployment
-const SOUND_CONTRACT_ADDRESS = "0xD784E5620fe63FFbEE470E7c3deCC149F50B2819";
+const SOUND_CONTRACT_ADDRESS = "0xF08e675f59Fc28414264E0f4b848bFa733366F5B";
 
 const NETWORKS = {
   mainnet: 1,
@@ -12,27 +12,75 @@ const RPC_ENDPOINT = {
 };
 
 const SOUND_CONTRACT_ABI = [
-  { inputs: [], stateMutability: "nonpayable", type: "constructor" },
   {
-    anonymous: false,
     inputs: [
       {
-        indexed: false,
+        internalType: "address",
+        name: "caller",
+        type: "address",
+      },
+      {
         internalType: "address",
         name: "contractAddress",
         type: "address",
       },
       {
-        indexed: false,
+        internalType: "uint256",
+        name: "tokenID",
+        type: "uint256",
+      },
+    ],
+    name: "NotOwner",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "dataOwner",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "caller",
+        type: "address",
+      },
+    ],
+    name: "OwnerMismatch",
+    type: "error",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "contractAddress",
+        type: "address",
+      },
+      {
+        indexed: true,
         internalType: "uint256",
         name: "tokenID",
         type: "uint256",
       },
       {
         components: [
-          { internalType: "address", name: "owner", type: "address" },
-          { internalType: "bytes", name: "dataHash", type: "bytes" },
-          { internalType: "string", name: "metadata", type: "string" },
+          {
+            internalType: "address",
+            name: "owner",
+            type: "address",
+          },
+          {
+            internalType: "bytes",
+            name: "dataHash",
+            type: "bytes",
+          },
+          {
+            internalType: "string",
+            name: "metadata",
+            type: "string",
+          },
         ],
         indexed: false,
         internalType: "struct OwnerData.Data",
@@ -44,33 +92,34 @@ const SOUND_CONTRACT_ABI = [
     type: "event",
   },
   {
-    anonymous: false,
     inputs: [
       {
-        indexed: false,
         internalType: "address",
         name: "contractAddress",
         type: "address",
       },
       {
-        indexed: false,
         internalType: "uint256",
         name: "tokenID",
         type: "uint256",
       },
-    ],
-    name: "DataRemoved",
-    type: "event",
-  },
-  {
-    inputs: [
-      { internalType: "address", name: "contractAddress", type: "address" },
-      { internalType: "uint256", name: "tokenID", type: "uint256" },
       {
         components: [
-          { internalType: "address", name: "owner", type: "address" },
-          { internalType: "bytes", name: "dataHash", type: "bytes" },
-          { internalType: "string", name: "metadata", type: "string" },
+          {
+            internalType: "address",
+            name: "owner",
+            type: "address",
+          },
+          {
+            internalType: "bytes",
+            name: "dataHash",
+            type: "bytes",
+          },
+          {
+            internalType: "string",
+            name: "metadata",
+            type: "string",
+          },
         ],
         internalType: "struct OwnerData.Data",
         name: "data",
@@ -84,16 +133,81 @@ const SOUND_CONTRACT_ABI = [
   },
   {
     inputs: [
-      { internalType: "address", name: "contractAddress", type: "address" },
-      { internalType: "uint256", name: "tokenID", type: "uint256" },
+      {
+        internalType: "address",
+        name: "contractAddress",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "tokenID",
+        type: "uint256",
+      },
+      {
+        internalType: "bytes",
+        name: "signature",
+        type: "bytes",
+      },
+      {
+        components: [
+          {
+            internalType: "address",
+            name: "owner",
+            type: "address",
+          },
+          {
+            internalType: "bytes",
+            name: "dataHash",
+            type: "bytes",
+          },
+          {
+            internalType: "string",
+            name: "metadata",
+            type: "string",
+          },
+        ],
+        internalType: "struct OwnerData.Data",
+        name: "data",
+        type: "tuple",
+      },
+    ],
+    name: "signedAdd",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "contractAddress",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "tokenID",
+        type: "uint256",
+      },
     ],
     name: "get",
     outputs: [
       {
         components: [
-          { internalType: "address", name: "owner", type: "address" },
-          { internalType: "bytes", name: "dataHash", type: "bytes" },
-          { internalType: "string", name: "metadata", type: "string" },
+          {
+            internalType: "address",
+            name: "owner",
+            type: "address",
+          },
+          {
+            internalType: "bytes",
+            name: "dataHash",
+            type: "bytes",
+          },
+          {
+            internalType: "string",
+            name: "metadata",
+            type: "string",
+          },
         ],
         internalType: "struct OwnerData.Data[]",
         name: "",
@@ -101,16 +215,6 @@ const SOUND_CONTRACT_ABI = [
       },
     ],
     stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      { internalType: "address", name: "contractAddress", type: "address" },
-      { internalType: "uint256", name: "tokenID", type: "uint256" },
-    ],
-    name: "remove",
-    outputs: [],
-    stateMutability: "nonpayable",
     type: "function",
   },
 ];
